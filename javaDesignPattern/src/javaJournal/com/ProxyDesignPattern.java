53805db0396fda35c7ef2d41dfc2c2508ed6f0d0
Bu Lin <bob_b_lin@kingdee.com>
2017-09-07 18:49:57 +0800

    代理设计模式

--- javaDesignPattern/src/javaJournal/com/ProxyDesignPattern.java
+++ javaDesignPattern/src/javaJournal/com/ProxyDesignPattern.java
@@ -1,7 +1,16 @@
 package javaJournal.com;
 
 /**
- * Created by kingdee on 2017/9/7.
+ * Created by Bob on 2017/9/7.
+ * Email:boblam502@gmail.com
+ * java代理设计目标
+ * 1.场景
+ * 2.用法
+ * 3.静态代理和动态代理(反射)
+ * 4.aop cglib
+ *
+ * refer:http://www.byteslounge.com/tutorials/jdk-dynamic-proxies
+ *
  */
 public class ProxyDesignPattern {
 }
